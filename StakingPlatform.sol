72ce968bc6ee9de4d489d938855adba6132d9f06
joshschcom <84538564+joshschcom@users.noreply.github.com>
2022-12-11 12:02:45 +0100

    Update StakingPlatform.sol
    Added the import for the erc20 logic which our contract needs aswell as a blank struct for the user to add their own staking position. Well probably create a mapping for/with the user created staked position, of whom the striuct would be needed.

--- StakingPlatform.sol
+++ StakingPlatform.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract Staking_Platform  {
+
+    struct addStakingPosition {
+        
+    }
     
 }
