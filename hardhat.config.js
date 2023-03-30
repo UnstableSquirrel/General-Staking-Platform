bee50ff1d8edd9431a19e4ce4a41f00828e5aa38
UnstableSquirrel <100873190+UnstableSquirrel@users.noreply.github.com>
2023-03-30 21:46:21 +0200

    Delete hardhat.config.js

--- hardhat.config.js
+++
@@ -1,7 +0,0 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('@openzeppelin/hardhat-upgrades');
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.18",
-};
